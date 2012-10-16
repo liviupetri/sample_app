a6598da67ada7e4a5925d96599620dcdbd7a08e8
Liviu Petri <liviu.petri@yahoo.com>
2012-10-16 23:18:19 +0300

    finished layout part

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,69 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-	# describe "GET /static_pages" do
-	# it "works! (now write some real specs)" do
-	#   # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-	#   get static_pages_index_path
-	#   response.status.should be(200)
-	# end
-	# end
+describe "Static pages" do
 
-	describe "StaticPages" do
-		describe "Home page" do
-			it "should have the content 'Sample App'" do
-				visit '/static_pages/home'
-				page.should have_content('Sample App')	
-			end
-			it "should have base title" do
-				visit '/static_pages/home'
-				page.should have_selector('title' , 
-					:text => 'RoR Tutorial')
-			end
-			it "should not have custom page_title" do
-				visit '/static_pages/home'
-				page.should_not have_selector('title',
-					:text => "| Home")
-			end
-		end
+  subject { page }
 
-		describe "Help Page" do
-			it "should have the content 'Help'" do
-				visit '/static_pages/help'
-				page.should have_content('Help')
-			end
-			it "should have title | Help" do
-				visit '/static_pages/help'
-				page.should have_selector('title',
-					:text => '| Help')
-			end
+  describe "Home page" do
+    before { visit root_path }
 
-		end
-		describe "About Page" do
-			it "should have the content 'About'" do
-				visit '/static_pages/about'
-				page.should have_content('About')
-			end
-			it "should have title | About" do
-				visit '/static_pages/about'
-				page.should have_selector('title',
-					:text => '| About')
-			end
-		end
-		describe "Contact Page" do
-			it "should have h1 'Contact'" do
-				visit '/static_pages/contact'
-				page.should have_selector('h1',
-					:text => 'Contact')
-			end
-			it "should have title '| Contact'" do
-				visit '/static_pages/contact'
-				page.should have_selector('title',
-					:text => '| Contact')
-				
-			end
-		end
-	end
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
 
+  describe "Help page" do
+    before { visit help_path }
 
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
